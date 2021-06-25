4e52bce8c969befa47952fee4e7470d852e94548
wilsonzackery16@gmail.com <wilsonzackery16@gmail.com>
2021-06-25 14:03:57 -0600

    Updated the flow of the app

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-function getParish(){
-    const e = document.getElementById("parish");
-    const location = e.options[e.selectedIndex].value;
-    localStorage.setItem("location", location);
-    window.location = "weatherpage.html";
+const parishCoordinates = {
+    Kingston : ["17.995147", "-76.7846006"],
+    SaintElizabeth : ["18.0499998", "-77.7833302"],
+    SaintMary : ["18.3166654", "-76.8999964"],
+    SaintAndrew : ["18.0166666", "-76.8999964"],
+    Clarendon : ["18.0", "-77.283"],
+    SaintAnn : ["18.2", "-77.467"],
+    SaintJames : ["18.3833318", "-77.8833298"],
+    SaintCatherine : ["18.0", "-77.0"],
+    Trelawny : ["18.3833318", "-77.6333308"],
+    Manchester : ["18.0499998", "-77.5333312"],
+    SaintThomas : ["17.8999964", "-76.4333316"],
+    Portland : ["18.1333328", "-76.5333312"],
+    Westmoreland : ["18.2333324", "-78.1499994"],
+    Hanover : ["18.417", "-78.133"]
+
 }
 
-function loadPage(){
-    const location = localStorage.getItem("location");
+function processInfo(){
+    const e = document.getElementById("parish");
+    const location = e.options[e.selectedIndex].value;
+
+    const landingPage = document.getElementById('landing-page')
+
+    landingPage.style.display = "none"; 
+
+    document.getElementsByClassName('loader')[0].style.display ="block"
+    
+    setTimeout(() => {
+        document.getElementsByClassName('loader')[0].style.display ="none"
+        document.getElementById('info').style.display = 'block';
+        var coord = parishCoordinates[`${location.replace(/ +/g, "")}`]
+        loadPage(coord[0], coord[1], location)
+    }, 4000)
+
+    
+    
+}
+
+function loadPage(latitude, longitude, location){
+    // const location = localStorage.getItem("location");
     const currentDay = document.getElementById("current-day");
     const currentWeather = document.getElementById("current-weather");
     const weatherDesc = document.getElementById("weather-description");
     const dailyWeather = document.getElementById("daily-weather-info");
 
-    var latitude, longitude;
 
-    if(location==="Kingston"){
-        latitude = "17.970473949925978"
-        longitude = "-76.77963894762368"
-    }
-    else if(location === "Portland"){
-        latitude = "18.141876432845624"
-        longitude = "-76.58169372894798"
-    }
 
     const apiKey = "a59944a7dfac4d02e97986183343ea57"
 
@@ -47,7 +70,7 @@ function loadPage(){
             const description = `
                 <p>${iconDescription.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())} | ${location}</p>
             `
-            currentDay .innerHTML = date;
+            currentDay.innerHTML = date;
             currentWeather.innerHTML = weather;
             weatherDesc.innerHTML = description;
             
@@ -72,4 +95,40 @@ function loadPage(){
             }
 
         })
+        .then(error => {
+
+        })
 }
+
+
+// localStorage.getItem(parish)
+//         .then(coords => {
+//            const { latitude, longitude } = coords;
+//            loadPage(latitude, longitude)
+//         })
+//         .catch(error => {
+
+//             const parishApiKey = "8d8b8e427afd3af726a82f704daa5a06"
+
+//             const parishURL = `http://api.positionstack.com/v1/forward
+//                                 ?access_key=${parishApiKey}
+//                                 &query=${parish}, Jamaica
+//                                 &country=JM
+//                                 &limit=1`
+
+//             fetch(parishURL)
+//                 .then(res => {
+//                     const results = res['data']['results'][0]
+                    
+//                     const lat = results['latitude']
+//                     const long = results['longitude']
+
+//                     localStorage.setItem(parish, { latitude : lat, longitude : long })
+
+//                     loadPage(lat, long)
+//                 })
+//                 .catch(error => {
+//                     console.log(error)
+//                 })
+
+        // })
